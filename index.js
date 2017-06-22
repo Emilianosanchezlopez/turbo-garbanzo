1047eae04a0469ed1888edadb518b086b56850e5
Emilianosanchezlopez <emilianosanchezlopez@hotmail.com>
2017-06-22 17:05:32 -0500

    http server

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var express = require('express');
 //   }
 // });
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 1337;
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
