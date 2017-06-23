288ba418b1178834d20558a046e93d74bf0011db
Emilianosanchezlopez <emilianosanchezlopez@hotmail.com>
2017-06-23 09:35:29 -0500

    Indexjs

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 var express = require('express');
-// var azure = require('azure-storage');
 
-// var tableService = azure.createTableService();
-// tableService.createTableIfNotExists('mytable', function(error, result, response) {
-//   if (!error) {
-//     // result contains true if created; false if already exists
-//   }
-// });
 var app = express();
-var port = process.env.PORT || 1337;
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-app.get('/', function (req, res) {
-   res.send("Hello from npm start");
- });
-app.get('/books', function (req, res) {
-   res.send("Hello from Books");
- });
+// app.get('/', function (req, res) {
+//    res.send("Hello from npm start");
+//  });
+// app.get('/books', function (req, res) {
+//    res.send("Hello from Books");
+//  });
 
-app.listen(5000, function (err) {
+var port = process.env.PORT || 5000;
+app.listen(port, function (err) {
   console.log('I\'am running on port '+ port);
 });
